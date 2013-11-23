9bbcb829b97288553e5c104d563ebf14c6435207
mosowski <mosowski@outlook.com>
2013-11-23 21:26:41 +0100

    perfab

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dropper : MonoBehaviour {
+
+    public float dropChance;
+    public string[] dropsPrefabs;
+    public float[] dropWeights;
+
+    void Start () {
+    
+    }
+    
+    void Update () {
+    
+    }
+}
