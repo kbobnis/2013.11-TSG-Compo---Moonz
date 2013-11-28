d7bf8c85d07b78dc28d955e199f42abdcf238f6d
mosowski <mosowski@outlook.com>
2013-11-29 00:57:41 +0100

    Inhalatory.

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -22,17 +22,29 @@ public class Missile : MonoBehaviour {
     void Update () {
         sc.MoveForward(speed * Time.deltaTime);
 
-        List<GameObject> enemies = World.GetOppositeCollection(team);
-        for (int i=enemies.Count-1; i>=0; --i) {
-            GameObject enemy = enemies[i];
-            if (sc.IsColliding(enemy)) {
-                float overkill = enemy.GetComponent<Critter>().TakeDamage(dmg);
-                if (overkill <= 0) {
-                    World.RemoveMissile(gameObject);
-                    return;
-                }
-            }
-        }
+		if (dmg < 0) { // zbijacz pocisków
+			List<GameObject> missiles = World.missiles;
+			for (int i=missiles.Count-1; i>=0; --i) {
+				GameObject mis = missiles[i];
+				if (mis != gameObject && sc.IsColliding(mis)) {
+					if (mis.GetComponent<Missile>().team != team) {
+						World.RemoveMissile(mis);
+					}
+				}
+			}
+		} else {
+			List<GameObject> enemies = World.GetOppositeCollection(team);
+			for (int i=enemies.Count-1; i>=0; --i) {
+				GameObject enemy = enemies[i];
+				if (sc.IsColliding(enemy)) {
+					float overkill = enemy.GetComponent<Critter>().TakeDamage(dmg);
+					if (overkill <= 0) {
+						World.RemoveMissile(gameObject);
+						return;
+					}
+				}
+			}
+		}
 
         life -= Time.deltaTime;
         if (life < 0) {
